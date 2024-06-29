b75025e79adc24894f76b9cc888a302fa7fe258b
dodoschnau <huangmeisin@gmail.com>
2024-06-30 01:00:12 +0800

    feat: add header & footer

--- app.js
+++ app.js
@@ -4,16 +4,17 @@ const handlebars = require('express-handlebars') // 引入 express-handlebars
 const session = require('express-session')
 const flash = require('connect-flash')
 const passport = require('./config/passport')
+const handlebarsHelpers = require('./helpers/handlebars-helpers')
+const { getUser } = require('./helpers/auth-helpers')
 
 const app = express()
 const port = process.env.PORT || 3000
 const SESSION_SECRET = 'secret'
 
 // 註冊 Handlebars 樣板引擎，並指定副檔名為 .hbs
-app.engine('hbs', handlebars({ extname: '.hbs' }))
+app.engine('hbs', handlebars({ extname: '.hbs', helpers: handlebarsHelpers }))
 // 設定使用 Handlebars 做為樣板引擎
 app.set('view engine', 'hbs')
-
 app.use(express.urlencoded({ extended: true }))
 
 app.use(session({
@@ -30,6 +31,7 @@ app.use(flash())
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages')
   res.locals.error_messages = req.flash('error_messages')
+  res.locals.user = getUser(req)
   next()
 })
 
