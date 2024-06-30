23e87f91fdeb373605c5ececbd5fd3439827ae05
dodoschnau <huangmeisin@gmail.com>
2024-06-30 19:05:06 +0800

    feat: user model add is_admin

--- helpers/auth-helpers.js
+++ helpers/auth-helpers.js
@@ -2,6 +2,11 @@ const getUser = req => {
   return req.user || null
 }
 
+const ensureAuthenticated = (req, res, next) => {
+  return req.isAuthenticated()
+}
+
 module.exports = {
-  getUser
+  getUser,
+  ensureAuthenticated
 }
--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -2,8 +2,9 @@ const express = require('express')
 const router = express.Router()
 
 const adminControllers = require('../../controllers/admin-controllers')
+const { authenticatedAdmin } = require('../../middleware/auth')
 
-router.get('/restaurants', adminControllers.getRestaurants)
+router.get('/restaurants', authenticatedAdmin, adminControllers.getRestaurants)
 
 router.use('/', (req, res) => res.redirect('/admin/restaurants'))
 
