1eb0e29d2fb9a3ddab605ebced8ba1f631ddb9e1
dodoschnau <huangmeisin@gmail.com>
2024-07-01 15:29:48 +0800

    feat: admin update restaurant

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -3,9 +3,11 @@ const router = express.Router()
 
 const adminControllers = require('../../controllers/admin-controllers')
 
-router.get('/restaurants', adminControllers.getRestaurants)
 router.get('/restaurants/create', adminControllers.createRestaurant)
+router.get('/restaurants/:id/edit', adminControllers.editRestaurant)
 router.get('/restaurants/:id', adminControllers.getRestaurant)
+router.put('/restaurants/:id', adminControllers.putRestaurant)
+router.get('/restaurants', adminControllers.getRestaurants)
 router.post('/restaurants', adminControllers.postRestaurants)
 
 router.use('/', (req, res) => res.redirect('/admin/restaurants'))
