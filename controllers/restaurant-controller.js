2945494e6fcf2a37eb8285076743d0a0a85b6dfd
dodoschnau <huangmeisin@gmail.com>
2024-07-06 00:16:46 +0800

    feat: add categories navbar on restaurants index page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -2,17 +2,24 @@ const { Restaurant, Category } = require('../models')
 
 const restaurantController = {
   getRestaurants: (req, res, next) => {
-    return Restaurant.findAll({
-      include: Category,
-      raw: true,
-      nest: true
-    })
-      .then(restaurants => {
+    const categoryId = Number(req.query.categoryId) || ''
+    return Promise.all([
+      Restaurant.findAll({
+        include: Category,
+        where: {
+          ...categoryId ? { categoryId } : {}
+        },
+        raw: true,
+        nest: true
+      }),
+      Category.findAll({ raw: true })
+    ])
+      .then(([restaurants, categories]) => {
         const data = restaurants.map(r => ({
           ...r,
           description: r.description.substring(0, 50) + '...'
         }))
-        res.render('restaurants', { restaurants: data })
+        res.render('restaurants', { restaurants: data, categories, categoryId })
       })
       .catch(err => next(err))
   },
