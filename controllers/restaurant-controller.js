92d83a0e202137182c07d920c889e2e30a781724
dodoschnau <huangmeisin@gmail.com>
2024-07-05 15:34:20 +0800

    feat: add dashboard and implement viewCounts

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -18,13 +18,24 @@ const restaurantController = {
   },
   getRestaurant: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, {
+      include: Category
+    })
+      .then(restaurant => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        return restaurant.increment({ viewCounts: 1 })
+      })
+      .then(restaurant => res.render('restaurant', { restaurant: restaurant.toJSON() }))
+      .catch(err => next(err))
+  },
+  getDashboard: (req, res, next) => {
+    return Restaurant.findByPk(req.params.id, {
       include: Category,
       raw: true,
       nest: true
     })
       .then(restaurant => {
         if (!restaurant) throw new Error("Restaurant didn't exist!")
-        res.render('restaurant', { restaurant })
+        res.render('dashboard', { restaurant })
       })
       .catch(err => next(err))
   }
