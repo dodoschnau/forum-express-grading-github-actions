12d579fe7f56b6ba12a5978dbbbdee4bf695bfa8
dodoschnau <huangmeisin@gmail.com>
2024-07-14 17:09:22 +0800

    feat: api signin add jwt token

---
+++ controllers/apis/user-controller.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken')
+
+const userController = {
+  signIn: (req, res, next) => {
+    try {
+      const userData = req.user.toJSON()
+      delete userData.password // 因安全問題使用者密碼不應該回傳前端，因此刪除密碼
+      const token = jwt.sign(userData, process.env.JWT_SECRET, { expiresIn: '30d' }) // 簽發 JWT，效期 30 天
+      res.json({
+        status: 'success',
+        data: {
+          token,
+          user: userData
+        }
+      })
+    } catch (err) {
+      next(err)
+    }
+  }
+}
+
+module.exports = userController
